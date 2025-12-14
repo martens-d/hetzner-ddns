1d1466d05014832e3903046f0bf386da322fcc15
martens-d <73710614+martens-d@users.noreply.github.com>
2025-12-14 14:12:57 +0100

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "==== ENVIRONMENT VARIABLES ===="
+echo "ZONE_NAME: $ZONE_NAME"
+echo "API_TOKEN: $API_TOKEN"
+echo "RECORD_TYPE: $RECORD_TYPE"
+echo "RECORD_NAME: $RECORD_NAME"
+echo "INTERVAL: $INTERVAL"
+echo "HETZNER_API_TYPE: $HETZNER_API_TYPE"
+echo "==============================="
+exec python hetzner_ddns.py
